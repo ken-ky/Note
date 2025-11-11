cdc5284c4342d8c8a2a4805269e08d96439b11e1
kenky <2440330186@qq.com>
2025-11-11 20:21:35 +0800

    test

--- C_CPP/boost_study/timer/comprehensive_test/free_credit_card.cpp
+++ C_CPP/boost_study/timer/comprehensive_test/free_credit_card.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #include <boost/date_time/gregorian/greg_date.hpp>
 #include <boost/date_time/gregorian/parsers.hpp>
 #include <boost/date_time/special_defs.hpp>
+using namespace boost::gregorian;
 
 // 处理时间组件
 #include <boost/date_time/posix_time/posix_time.hpp>
@@ -18,4 +19,26 @@ class credit_card {
     credit_card(const char *bname, int no)
         : // 构造函数
           bank_name(bname), bill_day_no(no) {}
+
+    int calc_free_days(date consume_day = day_clock::local_day()) const;
+
+    friend bool operator<(const credit_card &l, const credit_card &r) {
+        return l.calc_free_days() < r.calc_free_days(); // 比较免息期
+    }
 };
+
+int credit_card::calc_free_days(date consume_day) const {
+    date bill_day(consume_day.year(), consume_day.month(),
+                  bill_day_no); // 得到记账日
+
+    if (consume_day > bill_day) {
+        bill_day += months(1);
+    }
+    return (bill_day - consume_day).days() + 20;
+}
+
+int main() {
+    credit_card a("A bank", 25); // A银行的记账日是每月的25日
+    credit_card b("B bank", 12); // B银行的记账日是每月的12号
+    return 0;
+}
